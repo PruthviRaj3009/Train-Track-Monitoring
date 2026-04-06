dd2039c854a791e2f8688466dc8db7986b7513fa
PruthviRaj3009 <pruthvirajpawar12.pdp@gmail.com>
2026-04-06 17:30:45 +0530

    api

--- train_track_monitoring/lib/config/maps_config.dart
+++
@@ -1,8 +0,0 @@
-/// Google Maps Configuration
-///
-/// Replace "YOUR_GOOGLE_MAPS_API_KEY_HERE" with your actual API key.
-/// Get your API key from: https://console.cloud.google.com/google/maps-apis
-class MapsConfig {
-  static const String googleMapsApiKey =
-      "AIzaSyCLIzlH4SSeguQayFGbvR5qllkx3m-QU_A";
-}
